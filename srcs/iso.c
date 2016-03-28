513ea2907fc36867e4775da5b0cabae7cbec9784
nmougino <nmougino@student.42.fr>
2016-03-28 05:15:33 +0200

    Norme- srcs/iso.c

--- srcs/iso.c
+++ srcs/iso.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/27 03:25:13 by nmougino          #+#    #+#             */
-/*   Updated: 2016/03/27 05:56:14 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/03/28 05:08:58 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,38 +31,42 @@ static t_px	iso_getcurp(t_data *data, size_t x, int y)
 	return (curp);
 }
 
-void		iso(t_meta *meta, t_data *data)
+static void	iso_loop(t_meta *meta, t_data *data, t_px **line)
 {
-	t_px	**line;
 	int		y;
 	size_t	i;
 	size_t	prevsize;
 
 	y = 0;
-	i = 0;
 	prevsize = 0;
+	while (data && ((line[1] = (t_px*)malloc(sizeof(t_px) * (data->size + 1)))))
+	{
+		i = 0;
+		while (i < data->size)
+		{
+			line[1][i] = iso_getcurp(data, i, y);
+			if (i > 0)
+				draw_line(meta->img, line[1] + i, line[1] + i - 1);
+			if (i < prevsize)
+				draw_line(meta->img, line[1] + i, line[0] + i);
+			i++;
+		}
+		iso_free(line[0]);
+		line[0] = line[1];
+		prevsize = data->size;
+		data = data->next;
+		y++;
+	}
+}
+
+void		iso(t_meta *meta, t_data *data)
+{
+	t_px	**line;
+
 	if ((line = (t_px**)malloc(sizeof(t_px*) * 2)))
 	{
 		line[0] = NULL;
-		while (data && ((line[1] = (t_px*)malloc(sizeof(t_px) * (data->size + 1)))))
-		{
-			i = 0;
-			while (i < data->size)
-			{
-				line[1][i] = iso_getcurp(data, i, y);
-				printf("x: %d, y: %d, h: %f\n", line[1][i].x, line[1][i].y, line[1][i].color);
-				if (i > 0)
-					draw_line(meta->img, line[1] + i, line[1] + i - 1);
-				if (i < prevsize)
-					draw_line(meta->img, line[1] + i, line[0] + i);
-				i++;
-			}
-			iso_free(line[0]);
-			line[0] = line[1];
-			prevsize = data->size;
-			data = data->next;
-			y++;
-		}
+		iso_loop(meta, data, line);
 		iso_free(line[0]);
 	}
 }
